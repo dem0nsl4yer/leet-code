1546e1b351a4c1cbb3a0b70450d3024992327b4c
TheyCallMeSid <101993882+dem0nsl4yer@users.noreply.github.com>
2023-08-24 13:59:29 -0400

    Update 645-set-mismatch.py

--- Hash-Maps/645-set-mismatch.py
+++ Hash-Maps/645-set-mismatch.py
@@ -12,3 +12,24 @@ class Solution:
             missing = arr[0]
 
         return [duplicate, missing]
+    
+## Efficient solution using sets -deriviative of hashmaps. 
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        num_set = set()
+        missing = duplicate = -1
+
+        for num in nums:
+            if num in num_set:
+                duplicate = num
+            else:
+                num_set.add(num)
+
+        for i in range(1, n+1):
+            if i not in num_set:
+                missing = i 
+                break
+
+        return [duplicate, missing]
