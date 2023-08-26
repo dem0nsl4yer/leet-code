28d82bb9821a47707996c7c3976d01bc96662c58
TheyCallMeSid <101993882+dem0nsl4yer@users.noreply.github.com>
2023-08-25 23:45:23 -0700

    Update 739-daily-temperatures.py

--- Array/739-daily-temperatures.py
+++ Array/739-daily-temperatures.py
@@ -8,3 +8,20 @@ class Solution:
                 k[i] = 1
             else:
                 stack.append(t[i])
+
+# Finally solved:
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        k = [0]*n
+        t = temperatures 
+        stack = []
+        for i in range(n):
+            while(stack) and temperatures[i] > temperatures[stack[-1]]:
+                # looping through the index location 
+                prev_idx = stack.pop()
+                k[prev_idx] = i- prev_idx
+            stack.append(i)
+        return k
+
